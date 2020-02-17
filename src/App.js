b155dbc1e4d366c8b5a4255b1ca57ce4a05808d2
liyuting1010 <liyuting951010@163.com>
2020-02-17 23:21:05 +0800

    Hard coded return for button on click event.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    login() {
+        return axios.get("/login?username=Yuting1&password=password1").then(res => {
+            console.log(res.data)
+        });
+    }
+
+    register() {
+        return axios.post("/register?username=Yuting10&password=password10");
+    }
+
+    viewAllBooks() {
+        return axios.get("/getAll").then(res => {
+            console.log(res.data)
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Biblioteca system</h1>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <button type="button" className="Login App-button" onClick={this.login}>Login</button>
+                    <button type="button" className="Register App-button" onClick={this.register}>Register</button>
+                    <button type="button" className="AllBook App-button" onClick={this.viewAllBooks}>View All Books</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
